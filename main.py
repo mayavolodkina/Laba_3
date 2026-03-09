27929b86f737948b7b23d43bdb88c839d5905b5c
Майя <maya.volodkina06@yandex.ru>
2026-03-09 14:47:40 +0300

    first commit

--- main.py
+++ main.py
@@ -1,1 +1,17 @@
-print("Hello World")
+import tkinter as tk
+from tkinter import messagebox, ttk
+
+class UnitConverter:
+    def __init__(self, root):
+        # root - это главное окно нашего приложения
+        self.root = root
+        self.root.title("Конвертер физических величин")  # Заголовок окна
+
+        # НАСТРОЙКА ОКНА
+        self.root.resizable(True, True) # Изменение размера окна
+        self.root.geometry("400x300")
+        self.create_menu() # меню
+        self.create_widgets() # эл-ты интерфейса
+
+
+
